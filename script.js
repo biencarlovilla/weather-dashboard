491610501bb579933e1b2314e94facb3b8b6b6f1
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-09-29 19:36:53 -0700

    I added a click event for the search button and console log if it working

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 var APIKey = "&appid=b39f7b6e38230aa7f049f7d88383ccc9";
+
+$(document).ready(function () {
+    $("#searchBtn").click(function (event) {
+        event.preventDefault();
+
+        var searchName = $("#search").val().trim();
+        console.log(searchName);
+    })
+
+})
