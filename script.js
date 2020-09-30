d7449684dde68944b7a807a8a619de9f384d7a52
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-09-29 20:18:53 -0700

    I started working on ajax call and storaging it to local storage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var cityArray = [];
 var APIKey = "&appid=b39f7b6e38230aa7f049f7d88383ccc9";
 
 $(document).ready(function () {
@@ -5,7 +6,26 @@ $(document).ready(function () {
         event.preventDefault();
 
         var searchName = $("#search").val().trim();
-        console.log(searchName);
+        promptSearch(searchName);
+        //console.log(searchName);
     })
-
+    function promptSearch(citySearch) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + APIKey;
+        $("<button>").text(citySearch).prepend(".list-group-item");
+        $.ajax({
+            url: queryURL,
+            Method: "GET"
+        }).then(function (response) {
+            var cityPrevious = JSON.parse(localStorage.getItem("cities"));
+            if (cityPrevious) {
+                cityPrevious.push(response.name);
+                localStorage.setItem("cities", JSON.stringify(cityPrevious));
+            } else {
+                cityArray.push(response.name)
+                localStorage.setItem("cities", JSON.stringify(cityArray));
+                console.log(cityArray);
+            }
+    })
+    }
+    
 })
